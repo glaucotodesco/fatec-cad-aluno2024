b6e18dc7e2864e3b90a7d37b402ea322d3a1a165
glauco.todesco <glauco.todesco@gmail.com>
2024-04-02 11:06:34 -0300

    Remove and Update

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -20,5 +20,13 @@ export class StudentService {
     return this.http.post<Student>(this.url, student);
   }
 
+  delete(student:Student): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${student.id}`);
+  }
+
+  update(student:Student): Observable<Student>{
+    return this.http.put<Student>(`${this.url}/${student.id}`,student);
+  }
+
 
 }
